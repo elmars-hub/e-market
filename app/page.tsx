290c195c6b1e3c007aa4e148642d7f3697f27968
Martin Elmars <121293732+elmars-hub@users.noreply.github.com>
2025-10-07 12:39:18 +0100

    feat: slider

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,28 @@
+import CategoryList from "@/component/categorylist";
+import ProductList from "@/component/productList";
+import Slider from "@/component/slider";
+
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div className="">
+      <Slider />
+
+      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+        <h1 className="text-2xl">Featured Products</h1>
+        <ProductList />
+      </div>
+
+      <div className="mt-24 ">
+        <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">
+          Categories
+        </h1>
+        <CategoryList />
+      </div>
+
+      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+        <h1 className="text-2xl">New Products</h1>
+        <ProductList />
+      </div>
+    </div>
+  );
 }
