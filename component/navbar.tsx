d8ee647fcb4191defb11288a81821c2d57e2baa8
Martin Elmars <121293732+elmars-hub@users.noreply.github.com>
2025-10-07 12:38:56 +0100

    feat: navbar

---
+++ component/navbar.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import React from "react";
+import Menu from "./menu";
+import Image from "next/image";
+import SearchBar from "./searchbar";
+import NavBarIcons from "./navbarIcons";
+
+export default function Navbar() {
+  return (
+    <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
+      <div className="flex items-center justify-between h-full md:hidden">
+        {/* Mobile */}
+
+        <Link href="/">
+          <div className="text-2xl tracking-wide">NITA</div>
+        </Link>
+
+        <Menu />
+      </div>
+
+      {/* Desktop */}
+
+      <div className="hidden md:flex items-center h-full gap-8 justify-between">
+        {/* left */}
+        <div className="w-1/3 xl:w-1/2 flex items-center gap-12">
+          <Link href="/" className="flex items-center gap-3">
+            <Image src="/logo.png" alt="" width={24} height={24} />
+            <div className="text-2xl tracking-wide">NITA</div>
+          </Link>
+
+          <div className="hidden xl:flex gap-4">
+            <Link href="/">Homepage</Link>
+            <Link href="/">Shop</Link>
+            <Link href="/">Deals</Link>
+            <Link href="/">About</Link>
+            <Link href="/">Contact</Link>
+          </div>
+        </div>
+        {/* right */}
+        <div className="w-2/3 xl:w-1/2 flex items-center justify-between gap-8">
+          <SearchBar />
+          <NavBarIcons />
+        </div>
+      </div>
+    </div>
+  );
+}
