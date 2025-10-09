14dbdd85898675019956f86ed38b8600d14b708d
Martin Elmars <121293732+elmars-hub@users.noreply.github.com>
2025-10-09 13:12:24 +0100

    feat: filter

--- component/filter.tsx
+++ component/filter.tsx
@@ -1,6 +1,23 @@
-import React from "react";
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 export default function Filter() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const { replace } = useRouter();
+
+  function handleFilterChange(
+    e: React.ChangeEvent<HTMLSelectElement | HTMLInputElement>
+  ) {
+    const { name, value } = e.target;
+
+    const params = new URLSearchParams(searchParams);
+    params.set(name, value);
+    replace(`${pathname}?${params.toString()}`);
+  }
+
   return (
     <div className="mt-12 flex justify-between">
       <div className="flex gap-6 flex-wrap">
@@ -8,6 +25,7 @@ export default function Filter() {
           name="type"
           id=""
           className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED]"
+          onChange={handleFilterChange}
         >
           <option>Type</option>
           <option value="physical">Physical</option>
@@ -18,17 +36,20 @@ export default function Filter() {
           name="min"
           placeholder="min price"
           className="text-xs rounded-2xl pl-2 w-24 ring-1 ring-gray-400"
+          onChange={handleFilterChange}
         />
         <input
           type="text"
           name="max"
           placeholder="max price"
           className="text-xs rounded-2xl pl-2 w-24 ring-1 ring-gray-400"
+          onChange={handleFilterChange}
         />
         {/* TODO: Filter Categories */}
         <select
           name="cat"
           className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED]"
+          onChange={handleFilterChange}
         >
           <option>Category</option>
           <option value="">New Arrival</option>
@@ -47,6 +68,7 @@ export default function Filter() {
           name="sort"
           id=""
           className="py-2 px-4 rounded-2xl text-xs font-medium bg-white ring-1 ring-gray-400"
+          onChange={handleFilterChange}
         >
           <option>Sort By</option>
           <option value="asc price">Price (low to high)</option>
